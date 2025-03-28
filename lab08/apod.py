c0b4ca19aa0d606ed1b21ea9fb301ba2939d5509
jamesbigomero <jamesbigomero@gmail.com>
2025-03-28 19:37:50 -0400

    commit
    commit

---
+++ lab08/apod.py
@@ -0,0 +1,16 @@
+import requests
+
+API_URL = "https://api.nasa.gov/planetary/apod"
+API_KEY = "DEMO_KEY"
+
+def get_apod(date=None):
+    params = {"api_key": API_KEY}
+    if date:
+        params["date"] = date
+
+    response = requests.get(API_URL, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": f"Failed to fetch APOD. Status Code: {response.status_code}"}
